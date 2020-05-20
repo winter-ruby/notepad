8cb005def5f720d060c3cd5ab6cd75a7680bc543
Ruby_Student <testrubymail.mail.ru>
2020-05-20 23:51:04 +0300

    post class +ancestors

---
+++ post.rb
@@ -0,0 +1,27 @@
+class Post
+  def initialize
+    @created_at = Time.now
+    @text = nil
+  end
+
+  def read_from_console
+#todo
+  end
+  def to_string
+    #todo
+  end
+
+  def save
+    file = File.new(file_path, "w:UTF-8")
+
+    for items in to_strings do
+      file.puts(item)
+    end
+    file.close
+  end
+  def file_path
+    current_path = File.dirname(__FILE__ )
+    file_name = @created_at.strftime("#{self.class.name}_%Y-%m-%d_%H-%M-%S.txt")
+    return current_path + "/" + file_name
+  end
+end
